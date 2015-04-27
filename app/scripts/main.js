a825c2a78652acb29e946af992f64de2373a7b10
Lu, Jun <luj1985@gmail.com>
2015-04-28 02:09:22 +0800

    add sample page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,7 +5,40 @@ PanlinCap.module('Home', function(Home, PanlinCap, Backbone, Marionette) {
     template: PanlinCapTpl['templates/home.hbs']
   });
 
+  var AboutView = Marionette.ItemView.extend({
+    template: PanlinCapTpl['templates/about.hbs'],
+    className: 'description'
+  });
+
+  var InvestmentView = Marionette.ItemView.extend({
+    template: PanlinCapTpl['templates/investment.hbs'],
+    className: 'description'
+  });
+
+  var StaticController = Marionette.Controller.extend({
+    showHome: function() {
+      PanlinCap.mainRegion.show(new HomeView());
+    },
+    showAbout: function() {
+      PanlinCap.mainRegion.show(new AboutView());
+    },
+    showInvestMent: function() {
+      PanlinCap.mainRegion.show(new InvestmentView());
+    }
+  });
+
+
   PanlinCap.addInitializer(function() {
-    PanlinCap.mainRegion.show(new HomeView());
+
+    new Marionette.AppRouter({
+      appRoutes : {
+        'investment(/)': 'showInvestMent',
+        'about(/)': 'showAbout',
+        '(/)' : 'showHome'
+      },
+      controller: new StaticController()
+    });
+
+
   });
 });
