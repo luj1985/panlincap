f3ae43693fce9cb4d75056317c09effd402ccad1
Lu, Jun <luj1985@gmail.com>
2015-05-10 12:21:18 +0800

    add decorator images

---
+++ app/scripts/split.js
@@ -0,0 +1,14 @@
+(function($, flux) {
+
+  flux.transitions.split = function(fluxslider, opts) {
+    return new flux.transition(fluxslider, $.extend({
+      barWidth: 60,
+      setup: function() {
+
+      },
+      execute: function() {
+
+      }
+    }, opts));  
+  }
+})(jQuery, flux);
