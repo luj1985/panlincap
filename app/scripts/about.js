d1c1122a3ada92aa9f52d12c4d06d8ca2b198e36
Lu, Jun <luj1985@gmail.com>
2015-05-25 02:40:00 +0800

    update about page

--- app/scripts/about.js
+++ app/scripts/about.js
@@ -1,20 +1,63 @@
 PanlinCap.module('About', function(About, PanlinCap, Backbone, Marionette) {
   'use strict';
+  var PARTITION = .3;
 
   var AboutView = Marionette.ItemView.extend({
     template: PanlinCapTpl['templates/about/about.hbs'],
-    className: 'description about'
+    className: 'description about',
+    onShow : function () {
+      var width = $(window).width();
+      var size = this.$el.width();
+      var reveal = this.$('.reveal');
+      var margin = (width - size) / 2;
+      var left = (width * PARTITION) - margin;
+      reveal.css('left', left + 'px');
+    },
+    toggleReveal : function() {
+      this.$('.reveal').toggleClass('active');
+    },
+    triggers : {
+      'click .topic.detail' : 'expand'
+    }
   });
+
 
   var AboutBgView = Marionette.ItemView.extend({
     template : PanlinCapTpl['templates/about/aboutbg.hbs'],
-    className: 'slides about'
+    className: 'slides about',
+    updateSeparation : function() {
+      
+      var width = $(window).width(),
+          height = $(window).height();
+      var left = this.$('.left'),
+          right = this.$('.right');
+
+      left.css('clip', 'rect(0px,' + width * PARTITION + 'px,' + height + 'px,0px)');
+      right.css('clip', 'rect(0px,' + width + 'px,' + height + 'px,' + width * PARTITION+ 'px)');
+    },
+    toggleSplit : function() {
+      this.$('.right').toggleClass('split');
+    },
+    initialize : function() {
+      $(window).on('resize', this.updateSeparation);
+    },
+    onRender : function() {
+      this.updateSeparation();
+    },
+    onDestroy : function() {
+      $(window).off('reisze', this.updateSeparation);
+    }
   });
 
   var StaticController = Marionette.Controller.extend({
     showAbout: function() {
-      PanlinCap.mainRegion.show(new AboutView());
-      PanlinCap.bgRegion.show(new AboutBgView());
+      var view = new AboutView(), bg = new AboutBgView();
+      bg.listenTo(view, 'expand', function() {
+        bg.toggleSplit();
+        view.toggleReveal();
+      })
+      PanlinCap.mainRegion.show(view);
+      PanlinCap.bgRegion.show(bg);
     }
   });
 
