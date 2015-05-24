0f0e01e5c17891c905d180eb4aaa8b19600c7903
Lu, Jun <luj1985@gmail.com>
2015-05-24 17:56:36 +0800

    add cases page

--- app/scripts/case.js
+++ app/scripts/case.js
@@ -4,12 +4,18 @@ PanlinCap.module('Case', function(Case, PanlinCap, Backbone, Marionette) {
 
   var CasesView = Marionette.ItemView.extend({
     template: PanlinCapTpl['templates/case/cases.hbs'],
-    className: 'description'
+    className: 'cases'
+  });
+
+  var CasesBgView = Marionette.ItemView.extend({
+    template: PanlinCapTpl['templates/case/casesbg.hbs'],
+    className: 'cases'
   });
 
   var StaticController = Marionette.Controller.extend({
     showCases: function() {
       PanlinCap.mainRegion.show(new CasesView());
+      PanlinCap.bgRegion.show(new CasesBgView());
     }
   });
 
