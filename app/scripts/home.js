a9e4680f2f72c6303fcba0f89c17a8edd7f43036
Lu, Jun <luj1985@gmail.com>
2015-05-25 01:28:09 +0800

    use css animation as slider

--- app/scripts/home.js
+++ app/scripts/home.js
@@ -6,28 +6,9 @@ PanlinCap.module('Home', function(Home, PanlinCap, Backbone, Marionette) {
     className: 'home'
   });
 
-  function play() {
-    var flux = window.myFlux;
-    if (flux) {
-      flux.next('slide');
-    }
-  }
-
-  var playHandler;
-
   var HomeBg = Marionette.ItemView.extend({
     template: PanlinCapTpl['templates/home/homebg.hbs'],
-    className: 'slides home',
-    onShow: function() {
-      window.myFlux = new flux.slider('.slides.home', {
-        autoplay: false
-      });
-      playHandler = window.setInterval(play, 10000);
-    },
-    onDestroy: function() {
-      delete window.myFlux;
-      clearInterval(playHandler);
-    }
+    className: 'slides home'
   });
 
 
