fdd3df53f1c465f55dc70190b22a4c5108a808fc
Lu, Jun <luj1985@gmail.com>
2015-05-25 22:34:37 +0800

    update reveal page style

--- app/scripts/share.js
+++ app/scripts/share.js
@@ -1,17 +1,9 @@
 PanlinCap.module('Share', function(Share, PanlinCap, Backbone, Marionette) {
   'use strict';
 
-  var PARTITION = .3;
+  var CONTENT_WIDTH = 960;
 
   Share.SplitView = Marionette.ItemView.extend({
-    onShow : function () {
-      var width = $(window).width();
-      var size = this.$el.width();
-      var reveal = this.$('.reveal');
-      var margin = (width - size) / 2;
-      var left = (width * PARTITION) - margin;
-      reveal.css('left', left + 'px');
-    },
     toggleReveal : function() {
       this.$('.reveal').toggleClass('active');
     },
@@ -23,14 +15,16 @@ PanlinCap.module('Share', function(Share, PanlinCap, Backbone, Marionette) {
 
   Share.SplitBgView = Marionette.ItemView.extend({
     updateSeparation : function() {
-      
       var width = $(window).width(),
           height = $(window).height();
       var left = this.$('.left'),
           right = this.$('.right');
 
-      left.css('clip', 'rect(0px,' + width * PARTITION + 'px,' + height + 'px,0px)');
-      right.css('clip', 'rect(0px,' + width + 'px,' + height + 'px,' + width * PARTITION+ 'px)');
+      var marginLeft = (width - CONTENT_WIDTH) / 2;
+      var offset = marginLeft + 160;
+
+      left.css('clip', 'rect(0px,' + offset + 'px,' + height + 'px,0px)');
+      right.css('clip', 'rect(0px,' + width + 'px,' + height + 'px,' + offset + 'px)');
     },
     toggleSplit : function() {
       this.$('.right').toggleClass('split');
