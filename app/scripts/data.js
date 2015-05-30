33a3ccad349bcd49b2b8dee2bea5a0e3cba8d927
Lu, Jun <luj1985@gmail.com>
2015-05-31 01:10:32 +0800

    separate out navigation data

--- app/scripts/data.js
+++ app/scripts/data.js
@@ -1,3 +1,20 @@
+PanlinCap.reqres.setHandler('menus:fetch', function() {
+
+  var data = [
+    { text : '首页', link : '/' }, 
+    { text : '关于我们', menu : [ { text : '关于我们', link : '/about' }, { text : '特点/优势', link : '#' }] }, 
+    { text : '投资理念', menu : [ { text : '投资策略', link : '#' },  { text : '投资理念', link : '/investment' }, { text : '价值提升机制', link : '#' }]},
+    { text : '核心团队', menu : [ { text : '投资团队', link : '/team'} ]},
+    { text : '基金管理', link : '/founds' },
+    { text : '投资组合', menu : [ { text : '投资案例', link : '/cases' }]},
+    { text : '新闻中心', menu : [ { text : '公司新闻', link : '#' }, { text : '被投公司新闻', link : '#' }]},
+    { text : '联系我们', menu : [ { text : '招贤纳士', link : '#' }, { text : '联系我们', link : '#' }, { text : '商业计划书', link : '#'}]}
+  ];
+
+  return new Backbone.Model(data);
+});
+
+
 PanlinCap.reqres.setHandler('founds:fetch', function() {
   var data = [{
     issue : '壹期基金',
--- app/scripts/navigation.js
+++ app/scripts/navigation.js
@@ -1,17 +1,6 @@
 PanlinCap.module('Navigation', function(Navigation, PanlinCap, Backbone, Marionette) {
   'use strict';
 
-  var data = [
-    { text : '首页', link : '/' }, 
-    { text : '关于我们', menu : [ { text : '关于我们', link : '/about' }, { text : '特点/优势', link : '#' }] }, 
-    { text : '投资理念', menu : [ { text : '投资策略', link : '#' },  { text : '投资理念', link : '/investment' }, { text : '价值提升机制', link : '#' }]},
-    { text : '核心团队', menu : [ { text : '投资团队', link : '/team'} ]},
-    { text : '基金管理', link : '/founds' },
-    { text : '投资组合', menu : [ { text : '投资案例', link : '/cases' }]},
-    { text : '新闻中心', menu : [ { text : '公司新闻', link : '#' }, { text : '被投公司新闻', link : '#' }]},
-    { text : '联系我们', menu : [ { text : '招贤纳士', link : '#' }, { text : '联系我们', link : '#' }, { text : '商业计划书', link : '#'}]}
-  ];
-
   var MenuView = Marionette.ItemView.extend({
     template: PanlinCapTpl['templates/nav.hbs'],
     className: 'ui menu',
@@ -20,9 +9,8 @@ PanlinCap.module('Navigation', function(Navigation, PanlinCap, Backbone, Marione
     }
   });
 
-  var menus = new Backbone.Model(data);
-
   PanlinCap.addInitializer(function() {
+    var menus = PanlinCap.reqres.request('menus:fetch');
     PanlinCap.navRegion.show(new MenuView({ collection : menus }));
   });
 });
