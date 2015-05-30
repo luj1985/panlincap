003d75ab96514797f065951559759f9ebf0d06d6
Lu, Jun <luj1985@gmail.com>
2015-05-30 11:46:08 +0800

    remove hardcoded data

--- app/scripts/navigation.js
+++ app/scripts/navigation.js
@@ -1,7 +1,20 @@
 PanlinCap.module('Navigation', function(Navigation, PanlinCap, Backbone, Marionette) {
   'use strict';
 
-  var NavView = Marionette.ItemView.extend({
+  var data = { 
+    menu: [
+      { text : '首页', link : '/' }, 
+      { text : '关于我们', menu : [ { text : '关于我们', link : '/about' }, { text : '特点/优势', link : '#' }] }, 
+      { text : '投资理念', menu : [ { text : '投资策略', link : '#' },  { text : '投资理念', link : '/investment' }, { text : '价值提升机制', link : '#' }]},
+      { text : '核心团队', menu : [ { text : '投资团队', link : '/team'} ]},
+      { text : '基金管理', menu : [ { text : '一期基金', link : '#'}, { text : '二期基金', link : '#'}, { text : '三期基金', link : '#'}, { text : '四期基金', link : '#'}, { text : '五期基金', link : '#'} ]},
+      { text : '投资组合', menu : [ { text : '投资案例', link : '/cases' }]},
+      { text : '新闻中心', menu : [ { text : '公司新闻', link : '#' }, { text : '被投公司新闻', link : '#' }]},
+      { text : '联系我们', menu : [ { text : '招贤纳士', link : '#' }, { text : '联系我们', link : '#' }, { text : '商业计划书', link : '#'}]}
+    ]
+  };
+
+  var MenuView = Marionette.ItemView.extend({
     template: PanlinCapTpl['templates/nav.hbs'],
     className: 'ui menu',
     onRender : function() {
@@ -9,7 +22,9 @@ PanlinCap.module('Navigation', function(Navigation, PanlinCap, Backbone, Marione
     }
   });
 
+  var menus = new Backbone.Model(data);
+
   PanlinCap.addInitializer(function() {
-    PanlinCap.navRegion.show(new NavView());
+    PanlinCap.navRegion.show(new MenuView({model : menus}));
   });
 });
