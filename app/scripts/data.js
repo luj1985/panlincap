ae3c8a00e4ae19af2b7ce5f5465ea5bbc74c4ad6
Lu, Jun <luj1985@gmail.com>
2015-05-31 20:47:19 +0800

    update assets

--- app/scripts/data.js
+++ app/scripts/data.js
@@ -138,7 +138,7 @@
     var data = [
       { text : '首页', link : '/' }, 
       { text : '关于我们', link : '/about' }, 
-      { text : '投资理念', menu : [ { text : '投资策略', link : '#' },  { text : '投资理念', link : '/investment' }, { text : '价值提升机制', link : '#' }]},
+      { text : '投资理念', link : '/investment' },
       { text : '核心团队', menu : [ { text : '投资团队', link : '/team'} ]},
       { text : '基金管理', link : '/founds' },
       { text : '投资组合', menu : [ { text : '投资案例', link : '/cases' }]},
--- app/scripts/pages/invest.js
+++ app/scripts/pages/invest.js
@@ -2,25 +2,40 @@ PanlinCap.module('Invest', function(Invest, PanlinCap, Backbone, Marionette) {
   'use strict';
 
   var slogan = [
-    { text : '投资核心原则', link : '/investment/show' }, 
-    { text : '重点投资领域', link : '/investment/show' }, 
-    { text : '价值提升机制', link : '/investment/show' }
+    { text : '核心理念', link : '/investment/principle' }, 
+    { text : '投资策略', link : '/investment/strategy' }, 
+    { text : '价值提升机制', link : '/investment/price' }
   ];
 
-  var reveal = {
+  var principle = {
     back : '/investment',
     description : [
-      '<h2>投资核心原则</h2>',
-      '<h3>新兴产业</h3>',
-      '<p>紧密贴合国家政策所长期支持的产业创新升级的大趋势，精选拥有扎实基础并具备做大做强DNA的创新性民企；</p>',
-      '<h3>扎根价值</h3>',
-      '<p>从企业的高成长性中寻求高回报</p>',
-      '<h3>稳健交易</h3>',
-      '<p>控制投资成本，注重投资组合，力保投资安全</p>',
+      '<h3>核心投资理念 – “快乐投资”</h3>',
+      '<p>即被投企业获得理念认同和加速成功的快乐；投资人获得共同见证企业的高速成长，与合作伙伴分享财富倍增和事业跃升的快乐；而基金管理人则获得成就卓越、创造共赢、促进民族经济升级和创新的快乐。</p>'
+    ].join('\n')
+  };
+
+
+  var strategy = {
+    back : '/investment',
+    description : [
+      '<h3>投资核心原则</h3>',
+      '<p>新兴产业：紧密贴合国家政策所长期支持的产业创新升级的大趋势，精选拥有扎实基础并具备做大做强DNA的创新型民企</p>',
+      '<p>扎根价值：从企业的高成长性中寻求高回报</p>',
+      '<p>稳健交易：控制投资成本，注重投资组合，力保投资安全</p>',
       '<h3>重点投资领域</h3>',
-      '<p>医疗健康、节能环保、医疗健康、新的消费模式等新兴行业领域</p>',
+      '<p>医疗健康、节能环保、新的消费模式等新兴行业领域</p>',
       '<h3>投资阶段</h3>',
-      '<p>成长期为主，成熟期为辅。</p>'
+      '<p>GC（成长期）为主，VC为辅。</p>'
+    ].join('\n')
+  };
+
+  var price = {
+    back : '/investment',
+    description : [
+      '<h3>价值提升机制</h3>',
+      '<p>整合产业资源：基于专业的投资背景和过往的成功投资经验，我们在重点投资行业积累了丰富的行业资源，能够在被投企业的战略规划、市场开拓、人才引进等方面提供有效的帮助。</p>',
+      '<p>导入金融资源：我们将充分发挥自身的投行、财务、法律等方面的资源优势，帮助被投企业在后续融资、财务制度规范、资本运作、上市合规等方面获得明显进步。</p>'
     ].join('\n')
   };
 
@@ -41,23 +56,57 @@ PanlinCap.module('Invest', function(Invest, PanlinCap, Backbone, Marionette) {
       
       PanlinCap.vent.trigger('reveal:hide');
     },
-    showDetail : function() {
+    showPrice : function() {
       var layout = this.initializeLayout();
-      layout.getRegion('main').empty();
       layout.showChildView('sidebar', new Shared.SidebarView({ 
         collection : new Backbone.Collection(slogan) 
       }));
       layout.showChildView('breadcrumb', new Shared.BreadcrumbView({ 
         collection : new Backbone.Model([
           { text : '投资理念', link : '/investment' }, 
-          {text : '投资核心原则', link : '/investment/show'}
+          { text : '价值提升机制', link : '/investment/price'}
         ]) 
       }));
       layout.showChildView('main', new Shared.RevealView({ 
-        model : new Backbone.Model(reveal) 
+        model : new Backbone.Model(price) 
       }));
 
       PanlinCap.vent.trigger('reveal:active');
+    },
+    showPrinciple : function() {
+      var layout = this.initializeLayout();
+      layout.showChildView('sidebar', new Shared.SidebarView({ 
+        collection : new Backbone.Collection(slogan) 
+      }));
+      layout.showChildView('breadcrumb', new Shared.BreadcrumbView({ 
+        collection : new Backbone.Model([
+          { text : '投资理念', link : '/investment' }, 
+          { text : '投资核心原则', link : '/investment/principle'}
+        ]) 
+      }));
+      layout.showChildView('main', new Shared.RevealView({ 
+        model : new Backbone.Model(principle) 
+      }));
+
+      PanlinCap.vent.trigger('reveal:active');
+    },
+    showStrategy : function() {
+      var layout = this.initializeLayout();
+      layout.showChildView('sidebar', new Shared.SidebarView({ 
+        collection : new Backbone.Collection(slogan) 
+      }));
+      layout.showChildView('breadcrumb', new Shared.BreadcrumbView({ 
+        collection : new Backbone.Model([
+          { text : '投资理念', link : '/investment' }, 
+          { text : '投资策略', link : '/investment/strategy'}
+        ]) 
+      }));
+      layout.showChildView('main', new Shared.RevealView({ 
+        model : new Backbone.Model(strategy) 
+      }));
+
+      PanlinCap.vent.trigger('reveal:active');
+
     }
   });
 
@@ -66,7 +115,9 @@ PanlinCap.module('Invest', function(Invest, PanlinCap, Backbone, Marionette) {
     new Marionette.AppRouter({
       appRoutes : {
         'investment(/)': 'showInvestMent',
-        'investment/show' : 'showDetail'
+        'investment/principle(/)' : 'showPrinciple',
+        'investment/strategy(/)' : 'showStrategy',
+        'investment/price' : 'showPrice'
       },
       controller: new InvestmentController()
     });
