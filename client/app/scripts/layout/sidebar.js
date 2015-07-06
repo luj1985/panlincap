eb8ff0a91d700a47db66e3ff0c1860520a9b9d78
Lu, Jun <luj1985@gmail.com>
2015-07-06 21:00:41 +0800

    speedup scroll effect

--- client/app/scripts/layout/sidebar.js
+++ client/app/scripts/layout/sidebar.js
@@ -5,10 +5,10 @@ PanlinCap.module('Layout.Sidebar', function(Sidebar, PanlinCap, Backbone, Marion
     onRender : function() {
       var $wrapper = $('#body');
       function startScrollUp() {
-        $wrapper.animate({scrollTop: '-=30'}, 'slow', 'linear', startScrollUp);
+        $wrapper.animate({scrollTop: '-=50'}, 'normal', 'linear', startScrollUp);
       }
       function startScrollDown() {
-        $wrapper.animate({scrollTop: '+=30'}, 'slow', 'linear', startScrollDown); 
+        $wrapper.animate({scrollTop: '+=50'}, 'normal', 'linear', startScrollDown); 
       }
       function stopScrolling() {
         $wrapper.stop();
