096e77709ce944f232add65831ff80cf7a9d9533
Lu, Jun <luj1985@gmail.com>
2015-08-03 03:42:26 +0800

    update configs

--- panlincap/config/unicorn.rb
+++ panlincap/config/unicorn.rb
@@ -5,8 +5,8 @@ pid "/var/www/panlincap/panlincap/pids/unicorn.pid"
 stderr_path "/var/www/panlincap/panlincap/log/unicorn.err.log"
 stdout_path "/var/www/panlincap/panlincap/log/unicorn.out.log"
 
-listen "/tmp/unicorn.panlincap/panlincap.sock"
+listen "/tmp/unicorn.panlincap.sock"
 
-worker_processes 1
+worker_processes 2
 
 timeout 30
