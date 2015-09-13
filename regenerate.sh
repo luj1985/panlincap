5e821717be2bbe5b581d3b8d38b6e99aa845719e
Lu, Jun <luj1985@gmail.com>
2015-09-13 23:51:26 +0800

    run foreground

--- regenerate.sh
+++ regenerate.sh
@@ -2,4 +2,4 @@
 cd panlincap
 rm db/*.db
 bundle exec padrino rake db:create db:migrate db:seed -e production
-bundle exec padrino start -e production -h 0.0.0.0 &
+bundle exec padrino start -e production -h 0.0.0.0
