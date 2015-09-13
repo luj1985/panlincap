b52fde85c7c2e778a39c1ae01b3d5ba7a2746b78
Lu, Jun <luj1985@gmail.com>
2015-09-13 23:30:30 +0800

    eliminate hard coded html page

--- client/app/scripts/pages/contact.js
+++ client/app/scripts/pages/contact.js
@@ -3,28 +3,13 @@ PanlinCap.module('PanlinCap.Contact', function(Contact, PanlinCap, Backbone, Mar
 
   var Layout = PanlinCap.module('PanlinCap.Layout');
 
-  var AddressView = Marionette.ItemView.extend({
-    template : PanlinCapTpl['templates/contact/address.hbs'],
-    className : 'main-container address'
-  });
-
-  var HireView = Marionette.ItemView.extend({
-    template : PanlinCapTpl['templates/contact/hire.hbs'],
-    className : 'main-container hire'
-  });
-
-  var HireAccoutingView = Marionette.ItemView.extend({
-    template : PanlinCapTpl['templates/contact/accouting.hbs'],
-    className : 'main-container hire'
-  });
-
-  var HireAssistantView = Marionette.ItemView.extend({
-    template : PanlinCapTpl['templates/contact/assistant.hbs'],
-    className : 'main-container hire'
-  });
-
   var BizPlanView = Marionette.ItemView.extend({
     template : PanlinCapTpl['templates/contact/plan.hbs'],
+    className : 'main-container plan'
+  });
+
+  var HTMLView = Marionette.ItemView.extend({
+    template : Handlebars.compile('{{{body}}}'),
     className : 'main-container plan'
   });
   
@@ -33,22 +18,12 @@ PanlinCap.module('PanlinCap.Contact', function(Contact, PanlinCap, Backbone, Mar
     showContacts : function(subpage, id) {
       var layout = this.initializeLayout({className : 'sidebar-layout content'});
       layout.getRegion('main').empty();
-      if (id) {
-        if (id === 'accouting') {
-          layout.main.show(new HireAccoutingView());
-        } else if (id === 'assistant') {
-          layout.main.show(new HireAssistantView());
-        }
+      var name = id || subpage;
 
-      } else {
-        if (subpage === 'hire') {
-          layout.main.show(new HireView());
-        } else if (subpage === 'address') {
-          layout.main.show(new AddressView());
-        } else if (subpage === 'plan') {
-          layout.main.show(new BizPlanView());
-        }
-      }
+      PanlinCap.reqres.request('declaration:fetch', name).then(function(raw) {
+        var model = new Backbone.Model(raw);
+        layout.main.show(new HTMLView({ model : model }));
+      });
     }
   });
 
