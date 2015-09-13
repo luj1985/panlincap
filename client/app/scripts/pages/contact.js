77725abf0204ba76700e51337ba26defdf07269a
Lu, Jun <luj1985@gmail.com>
2015-09-13 23:51:06 +0800

    add null check

--- client/app/scripts/pages/contact.js
+++ client/app/scripts/pages/contact.js
@@ -18,12 +18,18 @@ PanlinCap.module('PanlinCap.Contact', function(Contact, PanlinCap, Backbone, Mar
     showContacts : function(subpage, id) {
       var layout = this.initializeLayout({className : 'sidebar-layout content'});
       layout.getRegion('main').empty();
+      
       var name = id || subpage;
-
-      PanlinCap.reqres.request('declaration:fetch', name).then(function(raw) {
-        var model = new Backbone.Model(raw);
-        layout.main.show(new HTMLView({ model : model }));
-      });
+      if (name) {
+        if (name === 'plan') {
+          layout.main.show(new BizPlanView());
+        } else {
+          PanlinCap.reqres.request('declaration:fetch', name).then(function(raw) {
+            var model = new Backbone.Model(raw);
+            layout.main.show(new HTMLView({ model : model }));
+          });
+        }
+      }
     }
   });
 
---
+++ regenerate.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd panlincap
+rm db/*.db
+bundle exec padrino rake db:create db:migrate db:seed -e production
+bundle exec padrino start -e production -h 0.0.0.0 &
