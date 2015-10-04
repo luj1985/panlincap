c45ebb601ae89c413e23dd332cf4b87f2d50da33
Lu, Jun <luj1985@gmail.com>
2015-10-04 13:16:54 +0800

    refine layouts

--- client/app/scripts/navigation.js
+++ client/app/scripts/navigation.js
@@ -2,23 +2,21 @@ PanlinCap.module('Navigation', function(Navigation, PanlinCap, Backbone, Marione
   'use strict';
 
   var MenuView = Marionette.ItemView.extend({
-    template: Handlebars.compile(
-      '{{#each items}}' + 
-      '<a href="#{{link}}">{{title}}</a>' +
-      '{{/each}}'
-    ),
-    className: 'wrapper'
+    template: Handlebars.compile('{{#each items}}<a href="#{{link}}">{{title}}</a>{{/each}}'),
+    className: 'menus'
   });
 
   PanlinCap.addInitializer(function() {
-    var promise = PanlinCap.reqres.request('menus:fetch', null);
-    promise.then(function(menus) {
-      PanlinCap.navRegion.show(new MenuView({ collection : new Backbone.Collection(menus) }));
+    PanlinCap.reqres.request('menus:fetch', null).then(function(menus) {
+      PanlinCap.navRegion.show(new MenuView({ 
+        collection : new Backbone.Collection(menus)
+      }));
     });
 
     $('.mobile.menu.trigger').click(function() {
       $('body').toggleClass('push');
     });
+
     $('#navigation').on('click', 'a', function() {
       $('body').removeClass('push');
     });
