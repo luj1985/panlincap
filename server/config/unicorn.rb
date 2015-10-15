7648f2c5f56a51b9ce72904e9d6fb1af8060fa3b
Lu, Jun <luj1985@gmail.com>
2015-10-15 22:48:31 +0800

    add unicorn configuration

--- server/config/unicorn.rb
+++ server/config/unicorn.rb
@@ -1,12 +1,32 @@
-working_directory "/home/panlincap/panlincap/server"
+# Help ensure your application will always spawn in the symlinked
+# "current" directory that Capistrano sets up.
+app_root = File.expand_path(".")
 
-pid "/home/panlincap/panlincap/server/pids/unicorn.pid"
+working_directory app_root
 
-stderr_path "/home/panlincap/panlincap/server/log/unicorn.err.log"
-stdout_path "/home/panlincap/panlincap/server/log/unicorn.out.log"
-
-listen "/tmp/unicorn.panlincap.sock"
-
+# Use at least one worker per core if you're on a dedicated server,
+# more will usually help for _short_ waits on databases/caches.
 worker_processes 2
 
+# nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
+
+# feel free to point this anywhere accessible on the filesystem
+pid "#{app_root}/pids/unicorn.pid"
+
+# By default, the Unicorn logger will write to stderr.
+# Additionally, ome applications/frameworks log to stderr or stdout,
+# so prevent them from going to /dev/null when daemonized here:
+stderr_path "#{app_root}/log/unicorn.err.log"
+stdout_path "#{app_root}/log/unicorn.out.log"
+
+# listen on both a Unix domain socket and a TCP port,
+# we use a shorter backlog for quicker failover when busy
+listen "/tmp/unicorn.panlincap.sock"
+listen 8081, :tcp_nopush => true
+
+# combine Ruby 2.0.0dev or REE with "preload_app true" for memory savings
+# http://rubyenterpriseedition.com/faq.html#adapt_apps_for_cow
+preload_app true
+GC.respond_to?(:copy_on_write_friendly=) and
+  GC.copy_on_write_friendly = true
