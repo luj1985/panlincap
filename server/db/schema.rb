a1c5c341f8b002788eb285e53a857212254a621b
Lu, Jun <luj1985@gmail.com>
2015-10-19 00:21:15 +0800

    add admin page module translations

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 9) do
+ActiveRecord::Schema.define(version: 11) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
@@ -85,6 +85,13 @@ ActiveRecord::Schema.define(version: 9) do
     t.string   "title"
     t.string   "link"
     t.integer  "parent"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "settings", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
