f3be641a8a3b8c94d7534f06185dfd904e15217f
Lu, Jun <luj1985@gmail.com>
2015-11-01 18:25:46 +0800

    use 64x64 favicon

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ rm -rf server/public/images
 rm -rf server/public/styles
 rm -rf server/public/scripts
 rm server/public/*.html
+rm server/public/*.png
 rm server/public/*.ico
 rm server/public/robots.txt
 
