89dedb4ce656a5d2e51019ba4f395b96982b4193
Lu, Jun <luj1985@gmail.com>
2015-11-02 20:31:56 +0800

    enrich html editor

--- server/public/admin/javascripts/declarations-editor.js
+++ server/public/admin/javascripts/declarations-editor.js
@@ -4,11 +4,14 @@ $(function() {
 
   $.Editable.DEFAULTS.key = 'rH-9mcbfD-13rscB1vyz==';
 
-
   var token = $('input[name="authenticity_token"]').val();
   $htmleditor.editable({
+    pasteImage: true,
+    pastedImagesUploadURL : "/admin/images/upload",
     preloaderSrc: "/images/preloader.gif",
     inlineMode: false,
+    toolbarFixed: false,
+    defaultImageAlignment: 'left',
     language: "zh_cn",
     mediaManager: true,
     imageUploadParams : { authenticity_token : token },
