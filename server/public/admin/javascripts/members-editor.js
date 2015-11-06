3383e4c05e221efbe01c12727bbc0f0a3085b34a
Lu, Jun <luj1985@gmail.com>
2015-11-07 01:35:15 +0800

    add avatar upload button

--- server/public/admin/javascripts/members-editor.js
+++ server/public/admin/javascripts/members-editor.js
@@ -17,4 +17,27 @@ $(function() {
       console.log(data);
     }
   });
+
+
+  var $progress = $('#progress'),
+      $img = $('#member-img'),
+      $imgfield = $('input[name="member[avatar]"]');
+
+  $('#upload').fileupload({
+    dataType : 'json',
+    paramName : 'file',
+    type : 'POST',
+    done : function(e, data) {
+      var result = data.result;
+      $imgfield.val(result.link);
+      $img.attr('src', result.link)
+    },
+    progressall: function (e, data) {
+      var progress = parseInt(data.loaded / data.total * 100, 10);
+      $progress.html(progress + "%");
+      if (progress === 100) {
+        $progress.html('');
+      }
+    }
+  });
 });
